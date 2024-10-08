9b550c94fe22da420500034a0311a1a7d289d169
ThanhPhong207 <2251120372@ut.edu.vn>
2024-10-08 15:25:29 +0700

    Delete src/main/java/org/example/nishiki_koi_shop/repository/FishRepository.java

--- src/main/java/org/example/nishiki_koi_shop/repository/FishRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.nishiki_koi_shop.repository;
-
-import org.example.nishiki_koi_shop.model.entity.Fish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FishRepository extends JpaRepository<Fish, Long> {
-}
