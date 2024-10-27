9e9642e9f3349b00d33f7f3cd7d334938a2631d2
NguyenLam1708 <lam170804@gmail.com>
2024-10-27 13:25:33 +0700

    write api for OrderTour

---
+++ src/main/java/org/example/nishiki_koi_shop/service/OrderTourService.java
@@ -0,0 +1,13 @@
+package org.example.nishiki_koi_shop.service;
+
+import org.example.nishiki_koi_shop.model.dto.OrderTourDto;
+import org.example.nishiki_koi_shop.model.payload.OrderTourForm;
+
+import java.util.List;
+
+public interface OrderTourService {
+    OrderTourDto createOrderTour(OrderTourForm orderTourForm);
+    OrderTourDto getOrderTourById(long id);
+    List<OrderTourDto> getAllOrderTours();
+    OrderTourDto updateOrderTour(long id, OrderTourForm orderTourForm);
+}
