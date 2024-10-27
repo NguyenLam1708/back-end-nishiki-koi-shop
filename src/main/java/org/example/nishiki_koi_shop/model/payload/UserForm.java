09015e6d768d0cb31e33729ca61accfe4a4efc84
NguyenLam1708 <lam170804@gmail.com>
2024-10-27 13:23:38 +0700

    fix UserForm

--- src/main/java/org/example/nishiki_koi_shop/model/payload/UserForm.java
+++ src/main/java/org/example/nishiki_koi_shop/model/payload/UserForm.java
@@ -1,17 +1,31 @@
 package org.example.nishiki_koi_shop.model.payload;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jdk.jshell.Snippet;
+import lombok.Builder;
 import lombok.Data;
+import org.example.nishiki_koi_shop.model.dto.UserDto;
+import org.example.nishiki_koi_shop.model.entity.User;
 
 import java.time.LocalDate;
 
+@Builder
 @Data
 public class UserForm {
     private String fullName;
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    private LocalDate dob;
+    private String username;
     private String email;
     private String phone;
     private String address;
-    private String profilePicture;
+
+    public static UserForm from(User user) {
+        return UserForm.builder()
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .phone(user.getPhoneNumber())
+                .address(user.getAddress())
+                .username(user.getUsername())
+                .build();
+    }
+
 }
