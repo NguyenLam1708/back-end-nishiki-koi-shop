e5541d1251f416d8debfa4ab1f41dbd6dce08cb4
NguyenLam1708 <lam170804@gmail.com>
2024-10-27 13:24:52 +0700

    write api for user

--- src/main/java/org/example/nishiki_koi_shop/controllers/UserController.java
+++ src/main/java/org/example/nishiki_koi_shop/controllers/UserController.java
@@ -2,13 +2,37 @@ package org.example.nishiki_koi_shop.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.nishiki_koi_shop.model.dto.UserDto;
+import org.example.nishiki_koi_shop.model.payload.ChangePasswordForm;
+import org.example.nishiki_koi_shop.model.payload.UserForm;
+import org.example.nishiki_koi_shop.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
+
+    @GetMapping("/myInfo")
+    public ResponseEntity<UserDto> getMyInfo() {
+        return ResponseEntity.ok(userService.getMyInfo());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserForm> updateUser(@PathVariable("id") Long id, @RequestBody UserForm form) {
+        return ResponseEntity.ok(userService.updateUser(id, form));
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordForm form, Principal connectedUser) {
+        return ResponseEntity.ok(userService.changePassword(form, connectedUser));
+    }
 
 
 }
