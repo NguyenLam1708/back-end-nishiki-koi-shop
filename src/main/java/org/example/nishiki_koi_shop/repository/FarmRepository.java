1d7e200920f7acb908dd6cb9b0b80ec2756aa225
ThanhPhong207 <2251120372@ut.edu.vn>
2024-10-28 23:03:00 +0700

    Delete src/main/java/org/example/nishiki_koi_shop/repository/FarmRepository.java

--- src/main/java/org/example/nishiki_koi_shop/repository/FarmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.nishikiKoiShop.repositories;
-
-import com.example.nishikiKoiShop.entities.Farm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FarmRepository extends JpaRepository<Farm, Long> {
-}
