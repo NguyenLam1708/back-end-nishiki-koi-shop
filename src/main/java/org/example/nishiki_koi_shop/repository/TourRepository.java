461ab47b4b652cce679444badb9fa820d5ceb1a2
NguyenLam1708 <lam170804@gmail.com>
2024-10-30 01:15:04 +0700

    add Optional<Tour> findByName(String name);

--- src/main/java/org/example/nishiki_koi_shop/repository/TourRepository.java
+++ src/main/java/org/example/nishiki_koi_shop/repository/TourRepository.java
@@ -4,6 +4,9 @@ import org.example.nishiki_koi_shop.model.entity.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TourRepository extends JpaRepository<Tour,Long> {
+    Optional<Tour> findByName(String name);
 }
