48866a502c6c2ca390eee739cd4f65d77df22fd9
NguyenLam1708 <lam170804@gmail.com>
2024-10-30 01:06:02 +0700

    create FishTypeRepository

---
+++ src/main/java/org/example/nishiki_koi_shop/repository/FarmRepository.java
@@ -0,0 +1,12 @@
+package org.example.nishiki_koi_shop.repository;
+
+import org.example.nishiki_koi_shop.model.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FarmRepository extends JpaRepository<Farm, Long> {
+    Optional<Farm> findByName(String name);
+}
---
+++ src/main/java/org/example/nishiki_koi_shop/repository/FishTypeRepository.java
@@ -0,0 +1,12 @@
+package org.example.nishiki_koi_shop.repository;
+
+import org.example.nishiki_koi_shop.model.entity.FishType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FishTypeRepository extends JpaRepository<FishType, Long> {
+    Optional<FishType> findByName(String name);
+}
