6c0a0019184635002bcf18a31649ccdd46981293
NguyenLam1708 <lam170804@gmail.com>
2024-11-22 21:53:23 +0700

    fix DataInitializer

--- src/main/java/org/example/nishiki_koi_shop/config/DataInitializer.java
+++ src/main/java/org/example/nishiki_koi_shop/config/DataInitializer.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 public class DataInitializer {
 
     @Bean
-    public CommandLineRunner initData(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public CommandLineRunner initData(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, CartRepository cartRepository) {
         return args -> {
             if (userRepository.findByEmail("lam1782004@gmail.com").isEmpty()){
                 Role userRole = roleRepository.findByName("ROLE_CUSTOMER")
@@ -31,6 +31,12 @@ public class DataInitializer {
                         .build();
 
                 userRepository.save(user);
+
+                Cart cart = Cart.builder()
+                        .user(user)
+                        .createdDate(LocalDate.now())
+                        .build();
+                cartRepository.save(cart);
             }
 
             if (userRepository.findByEmail("manager@example.com").isEmpty()) {
@@ -49,64 +55,15 @@ public class DataInitializer {
                         .build();
 
                 userRepository.save(admin);
-            }
 
-            // Create Sale Staff User
-            if (userRepository.findByEmail("salestaff@example.com").isEmpty()) {
-                Role saleStaffRole = roleRepository.findByName("ROLE_SALE_STAFF")
-                        .orElseGet(() -> roleRepository.save(Role.builder().name("ROLE_SALE_STAFF").build()));
-
-                User saleStaff = User.builder()
-                        .username("sale_staff")
-                        .email("salestaff@example.com")
-                        .password(passwordEncoder.encode("123456"))
-                        .fullName("Sale Staff User")
-                        .role(saleStaffRole)
+                Cart cart = Cart.builder()
+                        .user(admin)
                         .createdDate(LocalDate.now())
-                        .address("43/1")
-                        .phoneNumber("0362651807")
                         .build();
-
-                userRepository.save(saleStaff);
+                cartRepository.save(cart);
             }
 
-            // Create Delivery Staff User
-            if (userRepository.findByEmail("deliverystaff@example.com").isEmpty()) {
-                Role deliveryStaffRole = roleRepository.findByName("ROLE_DELIVERY_STAFF")
-                        .orElseGet(() -> roleRepository.save(Role.builder().name("ROLE_DELIVERY_STAFF").build()));
 
-                User deliveryStaff = User.builder()
-                        .username("delivery_staff")
-                        .email("deliverystaff@example.com")
-                        .password(passwordEncoder.encode("123456"))
-                        .fullName("Delivery Staff User")
-                        .role(deliveryStaffRole)
-                        .createdDate(LocalDate.now())
-                        .address("44/1")
-                        .phoneNumber("0362651808")
-                        .build();
-
-                userRepository.save(deliveryStaff);
-            }
-
-            // Create Consulting Staff User
-            if (userRepository.findByEmail("consultingstaff@example.com").isEmpty()) {
-                Role consultingStaffRole = roleRepository.findByName("ROLE_CONSULTING_STAFF")
-                        .orElseGet(() -> roleRepository.save(Role.builder().name("ROLE_CONSULTING_STAFF").build()));
-
-                User consultingStaff = User.builder()
-                        .username("consulting_staff")
-                        .email("consultingstaff@example.com")
-                        .password(passwordEncoder.encode("123456"))
-                        .fullName("Consulting Staff User")
-                        .role(consultingStaffRole)
-                        .createdDate(LocalDate.now())
-                        .address("45/1")
-                        .phoneNumber("0362651809")
-                        .build();
-
-                userRepository.save(consultingStaff);
-            }
         };
     }
 }
